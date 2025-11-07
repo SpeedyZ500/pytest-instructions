55f08cc63e6066608ab2973a414b352a967a98a4
Chase Odom <chasemichaelodom@gmail.com>
2025-11-06 23:14:15 -0700

    Adding comments & updating formatting

--- README.md
+++ README.md
@@ -136,26 +136,35 @@ This instruction will walk you through debugging `SongLibrary` using `pytest`.
 
     song_library = SongLibrary()
 
+    # ======== add_song tests ========
+
     def test_add_song_positive():
         song_library.clear()
         song_library.add_song(Song("We Built This City", "Rock"))
         assert 1 == len(song_library.songs)
 
+
     def test_add_song_negative():
         # TODO: Impelement
         raise NotImplementedError
+
+    # ======== remove_song tests ========
 
     def test_remove_song_positive():
         # TODO: Impelement
         raise NotImplementedError
 
+
     def test_remove_song_negative():
         # TODO: Impelement
         raise NotImplementedError
 
+    # ======== get_songs_in_genre tests ========
+
     def test_get_songs_in_genre_positive():
         # TODO: Impelement
         raise NotImplementedError
+
 
     def test_get_songs_in_genre_negative():
         # TODO: Impelement
--- solutions/song_library/test_song_library.py
+++ solutions/song_library/test_song_library.py
@@ -3,17 +3,22 @@ from song_library import Song, SongLibrary
 
 song_library = SongLibrary()
 
+# ======== add_song tests ========
+
 def test_add_song_positive():
     song_library.clear()
     song_library.add_song(Song("We Built This City", "Rock"))
 
     assert 1 == len(song_library.songs)
 
+
 def test_add_song_negative():
     song_library.clear()
 
     with pytest.raises(TypeError):
         song_library.add_song(27) # type: ignore
+
+# ======== remove_song tests ========
 
 def test_remove_song_positive():
     song_library.clear()
@@ -22,9 +27,12 @@ def test_remove_song_positive():
 
     assert 0 == len(song_library.songs)
 
+
 def test_remove_song_negative():
     song_library.clear()
     song_library.remove_song("The Unsung Song")
+
+# ======== get_songs_in_genre tests ========
 
 def test_get_songs_in_genre_positive():
     song_library.clear()
@@ -33,6 +41,7 @@ def test_get_songs_in_genre_positive():
     song_library.add_song(Song("Take Me Home, Country Roads", "Country"))
 
     assert 1 == len(song_library.get_songs_in_genre("Rock"))
+
 
 def test_get_songs_in_genre_negative():
     song_library.clear()
