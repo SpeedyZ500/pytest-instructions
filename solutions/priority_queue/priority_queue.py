448be4129c8da6f5057e225bdae42e22d3f4e8cb
Chase Odom <chasemichaelodom@gmail.com>
2025-11-06 22:13:10 -0700

    Adding solution bases

---
+++ solutions/priority_queue/priority_queue.py
@@ -0,0 +1,163 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+T = TypeVar('T')
+
+class PriorityQueue(ABC, Generic[T]):
+    """
+    Abstract class priority queue, has all functions that a priority queue requires, so that they must be implemented
+    ensures that things will be implemented correctly
+    
+    """
+    @abstractmethod
+    def build_queue(self, data:dict[T, float]) -> None:
+        pass
+
+    @abstractmethod
+    def insert (self, key:T, priority:float):
+        pass
+    @abstractmethod
+    def delete_min(self) -> T:
+        pass
+    @abstractmethod
+    def decrease_key(self, key:T, priority:float):
+        pass
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def __bool__(self) -> bool:
+        pass
+
+class BinaryHeapPQ(PriorityQueue[T]):
+    """
+    Class for a Binary Heap Priority Queue
+    This will hold a priority queue for all the data
+
+    Attributes:
+        T, the data type
+        __indexes(dict[T, int]) The indexes of the queue, allows us to improve decrease key insertion time.
+        __data(list[tuple[T, float]]) the queue, allows us to store data
+
+    """
+    __slots__ = ["__index","__data"]
+    def __init__(self)-> None:
+        self.__index:dict[T, int] = {}
+        self.__data:list[tuple[T, float]] = []
+    
+    def build_queue(self, data:dict[T, float]) -> None:
+        if not isinstance(data, dict):
+            raise TypeError
+        for key, priority in data.items():
+            self.insert(key, priority)
+    
+    def _swap(self, i:int, j:int) -> None:
+        n = len(self.__data)
+        if i < 0:
+            i += n
+        if j < 0:
+            j += n
+        self.__data[i], self.__data[j] = self.__data[j], self.__data[i]
+        self.__index[self.__data[i][0]] = i
+        self.__index[self.__data[j][0]] = j
+
+        
+    def _parent(self, index:int) -> int:
+        return (index-1) // 2
+    def _left(self, index:int) -> int:
+        return(index * 2) + 1
+    def _right(self, index:int) -> int:
+        return(index * 2) + 2
+    
+    def _bubble_up(self, index:int) -> None:
+        parent: int = self._parent(index)
+        while index > 0 and self.__data[index][1] < self.__data[parent][1]:
+            self._swap(index, parent)
+            index = parent
+            parent = self._parent(index)
+
+    
+    def insert(self, key:T, priority:float) -> None:
+        self.__index[key] = len(self.__data)
+        self.__data.append((key, priority))
+        self._bubble_up(self.__index[key])
+
+    def decrease_key(self, key:T, priority:float) -> None:
+       
+        if key not in self.__index.keys():
+            raise ValueError
+        self.__data[self.__index[key]] = (key, priority)
+        self._bubble_up(self.__index[key])
+
+    def _bubble_down(self, i:int) -> None:
+        n:int = len(self.__data)
+        while True:
+            min_i:int = i
+            l, r = self._left(i), self._right(i)  
+            if l < n and self.__data[l][1] < self.__data[min_i][1]:
+                min_i = l
+            if r < n and self.__data[r][1] < self.__data[min_i][1]:
+                min_i = r
+            if min_i == i:
+                break
+            self._swap(i, min_i)
+            i = min_i
+
+    def delete_min(self) -> T:
+        min_key, _ = self.__data[0]
+        last:tuple[T, float] = self.__data.pop()
+        self.__index.pop(min_key)
+        if self.__data:
+            self.__data[0] = last
+            self.__index[last[0]] = 0
+            self._bubble_down(0)
+        return min_key
+    
+    def __len__(self) -> int:
+        return len(self.__data)
+    def __bool__(self) -> bool:
+        return bool(self.__data)
+            
+
+
+
+class LinearPQ(PriorityQueue[T]):
+    """
+    Class for a Linear Priority Queue
+    This will hold a priority queue for all the data
+
+    Attributes:
+        T, the data type
+        __data(dict[T, float]) the queue, allows us to store data
+
+    """
+
+    __slots__ = ["__data"]
+    
+    def __init__(self)-> None:
+        self.__data:dict[T, float] = {}
+    
+    def build_queue(self, data:dict[T, float]) -> None:
+        if not isinstance(data, dict):
+            raise TypeError
+        self.__data.update(data)
+
+    def insert(self, key:T, priority:float) -> None:
+        self.__data[key] = priority
+    def decrease_key(self, key:T, priority:float) -> None:
+        if key not in self.__index.keys():
+            raise ValueError
+        self.__data[key] = priority
+    def delete_min(self) -> T:
+        min_key:T = list(self.__data.keys())[0]
+        min_value: float = self.__data[min_key]
+        for cur_key, cur_value in self.__data.items():
+            if min_value > cur_value:
+                min_value - cur_value
+                min_key = cur_key
+        self.__data.pop(min_key)
+        return min_key
+    def __len__(self) -> int:
+        return len(self.__data)
+    def __bool__(self) -> bool:
+        return bool(self.__data)
---
+++ solutions/song_library/song_library.py
@@ -0,0 +1,49 @@
+class Song:
+
+    def __init__(self, name:str, genre:str) -> None:
+        self.name = name
+        self.genre = genre
+    
+    def __repr__(self) -> str:
+        return f"\"{self.name}\""
+
+class SongLibrary:
+
+    songs:list[Song] = []
+
+    def __init__(self) -> None:
+        pass
+
+    def clear(self) -> None:
+        self.songs.clear()
+
+    def add_song(self, song:Song) -> None:
+
+        if not isinstance(song, Song):
+            raise TypeError
+
+        self.songs.append(song)
+        print(f"Added: {song.name}")
+
+
+    def remove_song(self, song_name:str) -> None:
+
+        for i in range(len(self.songs)):
+            if self.songs[i].name == song_name:
+                self.songs.pop(i)
+                print(f"Removed: {song_name}")
+                return
+        print("No song found")
+
+    def get_songs_in_genre(self, genre:str) -> list[Song]:
+
+        genre_songs:list[Song] = []
+
+        for i in range(len(self.songs)):
+            if self.songs[i].genre == genre:
+                genre_songs.append(self.songs[i])
+        
+        return genre_songs
+    
+    def __repr__(self) -> str:
+        return f"Library({repr(self.songs)})"
